d106d053a138f022a67aedabb488438d45916a50
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-02 09:56:33 -0600

    -- Start

--- MetaSimpleObservingExamples/ContentView.swift
+++ MetaSimpleObservingExamples/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Subscribe now")
+                .font(.system(size: 36,
+                              weight: .light,
+                              design: .serif))
+                .italic()
         }
         .padding()
     }
