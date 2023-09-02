62b55410231bf1613dfe71fa14fef888e4de1d79
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-02 09:49:51 -0600

    Initial Commit

---
+++ MetaSimpleObservingExamples/MetaSimpleObservingExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaSimpleObservingExamplesApp.swift
+//  MetaSimpleObservingExamples
+//
+//  Created by Mitch Andrade on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaSimpleObservingExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
